8e8a9835d24e28ec4aacd090cfc06520ea2cb927
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-12-16 17:31:02 +0200

    fixed untracked files

--- cmake-build-uno/CMakeFiles/PLATFORMIO_BUILD_DEBUG.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PLATFORMIO_BUILD_DEBUG"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PLATFORMIO_BUILD_DEBUG.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-uno/CMakeFiles/PLATFORMIO_DEVICE_LIST.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PLATFORMIO_DEVICE_LIST"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PLATFORMIO_DEVICE_LIST.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
