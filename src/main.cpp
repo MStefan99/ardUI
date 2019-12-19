6863ed6e07f464666935cab581fada81866650e0
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2019-12-19 13:32:04 +0200

    fixed config file

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,7 @@
 //
 
 #include <Arduino.h>
-#include "ardUI_setup.h"
-
-
-class mainScreen : public screen {
-    void onCreate() override {
-        screen::onCreate();
-        textView tv1;
-        mainScreen::addView(&tv1);
-        tv1.setOnClickListener([](view* view) {
-            ((textView*)view)->setText("TextView Clicked!");
-        });
-    }
-};
+#include "ardUI.h"
 
 
 void setup() {
